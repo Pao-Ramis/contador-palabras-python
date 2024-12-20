c60e120199745fe80d98c90a94e7599c1347f963
Pao <pao.ramis@gmail.com>
2024-12-20 17:22:58 -0300

    Realizamos un contador de palabras en base a una práctica de algoritmos en el cual usamos pseudocódigos y diagrama de flujo

---
+++ Contador-letreas.py
@@ -0,0 +1,11 @@
+# Paso 1: Indicar al usuario que ingrese una palabra
+
+palabra_ingresada = input("Por favor, ingrese la palara: ")
+
+# Paso 2: Conatríamos la cantidad de letras de la palabra ingresada
+
+cantidad_letras = len(palabra_ingresada)
+
+# Paso 3: Mostraremos al usuario la cantidad de letras de la palabra ingresada
+
+print("La palabra ingresada tiene", cantidad_letras, "letras.")
